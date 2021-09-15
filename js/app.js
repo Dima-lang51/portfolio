c53155e29223eee8fa9d98027121d9231e16c148
Dima-lang51 <68560471+Dima-lang51@users.noreply.github.com>
2021-09-15 22:50:08 +0300

    Home stretch
    1. Made modal windows open and closed.
    2. Started doing responsive layout.

---
+++ js/app.js
@@ -0,0 +1,62 @@
+$(function() {
+
+
+  /* Filter 
+  =======================*/
+  let filtr = $('[data-filter]');
+  filtr.on('click', function(event) {
+    event.preventDefault();
+
+    let category = $(this).data('filter');
+
+    if(category == 'all') {
+      $('[data-cat]').removeClass('hide');
+    } else {
+       $('[data-cat]').each(function() {
+      let workCat = $(this).data('cat');
+
+      if(workCat != category) {
+        $(this).addClass('hide');
+      } else {
+        $(this).removeClass('hide');
+      }
+    });
+    }
+  });
+
+
+  /* Modal 
+  =======================*/
+  let modalCall = $('[data-modal]');
+  let modalClose = $('[data-close]');
+
+  modalCall.on('click', function(event) {
+    event.preventDefault();
+    let $this = $(this);
+    let modalId = $this.data('modal');
+
+    $(modalId).addClass('show');
+    $('body').addClass('no-scroll');
+  });
+
+
+
+  modalClose.on('click', function(event) {
+    event.preventDefault();
+    let $this = $(this);
+    let modalParent = $this.parents('.modal');
+
+    modalParent.removeClass('show');
+    $('body').removeClass('no-scroll');
+  });
+
+
+   $('.modal').on('click', function(event) {
+    $(this).removeClass('show');
+    $('body').removeClass('no-scroll');
+  });
+
+   $('.modal__dialog').on('click', function(event) {
+     event.stopPropagation();
+  });
+});
